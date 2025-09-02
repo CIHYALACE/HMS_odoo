3e10761ae3dc6d18722c85562669b8673821ac59
AbdelRahman <abodyoussef2015@gmail.com>
2025-09-02 12:58:37 +0300

    Add state logging functionality and enhance patient form fields

--- models/hms_patient.py
+++ models/hms_patient.py
@@ -30,7 +30,7 @@ class HmsPatient(models.Model):
     history = fields.Html()
     cr_ratio = fields.Float()
     blood_type = fields.Selection(BLOOD_TYPES)
-    pcr = fields.Boolean()
+    pcr = fields.Boolean(default=True)
     image = fields.Binary()
     address = fields.Text()
     age = fields.Integer()
@@ -38,12 +38,53 @@ class HmsPatient(models.Model):
     department_id=fields.Many2one(comodel_name="hms.department")
     department_capacity=fields.Integer(related="department_id.capacity")
     doctor_id=fields.Many2many(comodel_name="hms.doctor")
+    state_log_ids = fields.One2many(
+        comodel_name="hms.patient.state.log",
+        inverse_name="patient_id",
+        string="State Logs"
+    )
 
-    api.onchange('state')
-    def state_change(self):
-        for rec in self:
-            vars={
-                'description': 'State Changed to %s'%(rec.state),
-                'patient_id' : rec.id
+    @api.onchange('state')
+    def _onchange_state(self):
+        if self.state:
+            return {
+                'warning': {
+                    'title': "State Changed",
+                    'message': f"State has been changed to: {self.state}",
+                    'type': 'notification',
+                }
             }
-            self.env['hms.patient.state.log'].create(vars)
+
+    def write(self, vals):
+        if 'state' in vals:
+            for rec in self:
+                rec.env['hms.patient.state.log'].create({
+                    'description': f"State Changed to {vals['state']}",
+                    'patient_id': rec.id
+                })
+        return super().write(vals)
+
+    def create(self, vals):
+        patient = super().create(vals)
+        if 'state' in vals:
+            patient.env['hms.patient.state.log'].create({
+                'description': f"State Changed to {vals['state']}",
+                'patient_id': patient.id
+            })
+        return patient
+
+    @api.onchange('departmnet_id')
+    def department_change(self):
+        if self.department_id == 'Null':
+            return 
+    
+    @api.onchange('age')
+    def age_change(self):
+        if self.age and self.age < 30:
+            self.pcr = True
+            return{
+            'warning':{
+                'title':"Warning",
+                'message':"The Pcr Setted True Cause The Edge Under 30"
+                }
+            }
